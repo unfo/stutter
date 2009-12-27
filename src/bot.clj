bbbb5b8cc9289651e5db01c66f565138f48e257e
Jan Wikholm <jw@jw.fi>
2009-12-27 15:58:14 +0200

    Connecting + PING PONG now works

--- src/bot.clj
+++ src/bot.clj
@@ -2,36 +2,49 @@
 (import '(java.net URL Socket))
 
 
-(defstruct ircbot :nick :input :output :channels)
+(defstruct ircbot :nick :server :input :output :channels)
 (defstruct message :text :output)
 
-(defn end-of-motd? [line] (re-matches #".*376.*" line))
-(defn ping? [line] (re-matches #".*PING.*" line))
+(defn end-of-motd? [line] (re-matches #".* 376 .*" line))
+(defn ping? [line] (re-matches #"PING .*" line))
 
 (defn write-to-server [message]
   (let [line (:text message)
         output (:output message)]
-  (do (.println output line)
+    (do
+      (println "-> " line)
+      (.println output line)
       (.flush output))))
 
-(defn react-to-message [line output]
+(defn react-to-message [line server output]
   (do (println "<- " line)
-      (if (ping? line) (write-to-server (struct message "PONG" output)))))
-
-
+      (cond
+          (ping? line) (write-to-server (struct message (str "PONG " server) output))
+        )))
 
 (defn react-to-messages [bot]
   (let [line (.readLine (:input bot))
-        output (:output bot)]
-    (react-to-message line output))
+        output (:output bot)
+        server (:server bot)]
+    (react-to-message line server output))
     (recur bot))
 
-(defn start-bot [nick, server, port, channels]
+(defn send-nick [nick output]
+  (write-to-server (struct message (str "NICK " nick) output)))
+(defn send-user [server output]
+    (write-to-server (struct message (str "USER stutter stutter " server " :Stutter Bot") output)))
+
+(defn start-bot [nick, #^String server, #^Integer port, channels]
   (println (str "Trying to create a bot with nick=" nick "; server=" server "; port=" port "; channels=" channels))
   (let [socket (Socket. server port)
         input (BufferedReader. (InputStreamReader. (.getInputStream socket)))
         output (PrintWriter. (.getOutputStream socket))
-        bot (struct ircbot nick input output channels)]
-    (react-to-messages bot)))
+        bot (struct ircbot nick server input output channels)]
+    (do
+    (send-nick nick output)
+    (send-user server output)
+    (react-to-messages bot)
+      )))
 
 
+ 
