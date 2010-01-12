1814f5dbeb9617d40ea34497283adfe2dbf01982
Jan Wikholm <jw@jw.fi>
2010-01-12 21:13:45 +0200

    (map fn coll) is *LAZY* -- thanks #clojure @ freenode :-)

--- src/bot.clj
+++ src/bot.clj
@@ -3,18 +3,43 @@
 
 
 (defstruct ircbot :nick :server :input :output :channels)
-(defstruct message :text :output)
+(defstruct private-command :nick :command)
+(defstruct public-command :nick :command :channel)
 
 (def urls (ref ()))
-(def bot (ref ircbot))
+(def bot (atom {}))
 (defn end-of-motd? [line] (re-matches #".* 376 .*" line))
 (defn ping? [line] (re-matches #"PING .*" line))
-(defn match-private-message [line] (re-find (re-pattern (str ":([^!]+).+? PRIVMSG " (:nick @bot) " :(.*)")) line))
-(defn match-channel-message [line] (re-find #".+? PRIVMSG (#[^ ]+) :(.*)" line))
-(defn match-spotify-uri [message] (re-find #".*spotify:(track|album|artist|user):([\w:]+).*" message))
-(defn match-spotify-url [message] (re-find #".*http://open.spotify.com/(track|album|artist|user)/([\w/]+).*" message))
-(defn match-part-request [message] (re-find #".*GTFO NIGGA.*" message))
-(defn match-join-request [message] (re-find #".*YO JOIN (#[^ ]+) KTHXBAI.*" message))
+(def command-char \,)
+
+(def example-private-message ":ircdude!username@dynamic-ip.isp.com PRIVMSG iStutter :np ircdude")
+(def example-channel-message ":ircdude!username@dynamic-ip.isp.com PRIVMSG #mychannel :,(join (channel #myotherchannel))")
+
+(defn match-private-message [line]
+  (let [[_ sender-nick command] (re-find (re-pattern (str ":([^!]+).+? PRIVMSG " (:nick @bot) " :(.*)")) line)]
+    (if (some nil? [sender-nick, command])
+      false
+      (struct private-command sender-nick command))))
+
+(defn match-channel-message [line]
+  (let [[_ sender-nick channel command] (re-find (re-pattern (str ":([^!]+).+? PRIVMSG (#[^ ]+) :" command-char "(.*)")) line)]
+    (if (some nil? [sender-nick, channel, command])
+      false
+      (struct public-command sender-nick command channel))))
+
+
+(defn match-part-request [message] (not (nil? (re-find #".*\(leave\).*" message))))
+(defn match-join-request [message]
+  (let [[_ channel] (re-find #".*\(join \(channel (#[^ ]+)\)\).*" message)]
+    (if (nil? channel)
+      false
+      channel)))
+
+
+(comment
+  (defn match-spotify-uri [message] (re-find #".*spotify:(track|album|artist|user):([\w:]+).*" message))
+  (defn match-spotify-url [message] (re-find #".*http://open.spotify.com/(track|album|artist|user)/([\w/]+).*" message))
+  )
 
 
 (defn write-to-server [line]
@@ -31,55 +56,49 @@
 
 (defn send-nick []
   (write-to-server (str "NICK " (:nick @bot))))
+
 (defn send-user []
   (write-to-server (str "USER stutter stutter " (:server @bot) " :Stutter Bot")))
-(defn join-channel [channel]
-  (println "join-channel [" channel "]")
-  (write-to-server (str "JOIN " channel)))
-(defn part [channel]
-  (write-to-server (str "PART " channel)))
+
+(defn join-channel [channel] (write-to-server (str "JOIN " channel)))
+(defn part-channel [channel] (write-to-server (str "PART " channel)))
 
 
 
 (defn respond-to [target message]
   (println "respond-to " target " ; " message)
   (write-to-server (str "PRIVMSG " target " :" message)))
-(defn resolve-spotify-uri-info [spotify-uri origin]
-  (println "resolve-spotify-uri-info")
-  (respond-to origin "Spotify support is incomplete"))
-(defn resolve-spotify-url-info [spotify-url origin]
-  (println "resolve-spotify-url-info")
-  (respond-to origin "Spotify support is incomplete"))
 
 
-(defn react-to [matches]
-  (let [[_ origin message] matches
-        spotify-uri (match-spotify-uri message)
-        spotify-url (match-spotify-url message)
-        channel-to-be-joined (match-join-request message)
-        channel-to-be-parted (match-part-request message)]
+(defn react-to-privately [command]
+  (let [channel-to-be-joined (match-join-request (:command command))]
     (cond
-      (not (nil? spotify-uri)) (resolve-spotify-uri-info spotify-uri origin)
-      (not (nil? spotify-url)) (resolve-spotify-url-info spotify-url origin)
-      (not (nil? channel-to-be-joined)) (join-channel (first (rest channel-to-be-joined)))
-      (not (nil? channel-to-be-parted)) (part origin))
-    ))
+      (not (false? channel-to-be-joined)) (join-channel channel-to-be-joined)
+      )))
 
-(defn join-startup-channels []
-  (println "joining" (:channels @bot))
-  (map #(join-channel %) (:channels @bot))
-  )
+(defn react-to-publicly [command]
+  (let [
+    channel-to-be-joined (match-join-request (:command command))
+    part-requested (match-part-request (:command command))
+    ]
+    (cond
+      (not (false? channel-to-be-joined)) (join-channel channel-to-be-joined)
+      (true? part-requested) (part-channel (:channel command))
+      )))
+
+(defn join-startup-channels "force traversing channel list and mapping" [] (dorun (map join-channel (:channels @bot))))
+
 (defn react-to-message [line]
   (let [
-        privmsg (match-private-message line)
-        chanmsg (match-channel-message line)
-        ]
+    privmsg (match-private-message line)
+    chanmsg (match-channel-message line)
+    ]
     (do (println "<- " line)
       (cond
         (ping? line) (write-to-server (str "PONG " (:server @bot)))
         (end-of-motd? line) (join-startup-channels)
-        (not (nil? privmsg)) (react-to privmsg)
-        (not (nil? chanmsg)) (react-to chanmsg)
+        (not (false? privmsg)) (react-to-privately privmsg)
+        (not (false? chanmsg)) (react-to-publicly chanmsg)
         ))))
 
 
@@ -89,7 +108,7 @@
       (println "react-to-messages line is nil")
       (do
         (react-to-message line)
-      ))
+        ))
     (recur)))
 
 (defn start-bot [botnick, #^String server, #^Integer port, channels]
@@ -97,21 +116,21 @@
   (let [socket (Socket. server port)
         input (BufferedReader. (InputStreamReader. (.getInputStream socket)))
         output (PrintWriter. (.getOutputStream socket))
-        newbot (struct ircbot botnick server input output (re-seq #"#[^ ]+" channels))]
+        chan-seq (re-seq #"#[^ ]+" channels)]
     (if (nil? socket)
       (println "Socket is not up?")
       (do
-        (dosync
-          (ref-set bot newbot))
+        (swap! bot assoc :nick botnick :server server :input input :output output :channels chan-seq)
         (send-nick)
         (send-user)
 
         (try
           (react-to-messages)
-          (catch NullPointerException e ((println  "Caught nullpointer: " (.getMessage e))))
+          (catch NullPointerException e ((println "Caught nullpointer: " (.getMessage e))))
           (finally (.close socket))
-          )
-        ))))
+          )))))
+
 (start-bot "iStutter" "irc.inet.fi" 6667 "#paskamaja #paskamaja2")
+
 
  
